3120e44c6db8000e26829163b7307f6c935cae4a
Napat Wannachai <napatwannachai@hotmail.com>
2016-11-17 00:30:18 +0700

    Add controller addcontact

--- todos/app.js
+++ todos/app.js
@@ -9,4 +9,18 @@ angular.module('ContactApp', [])
   self.add =  function(contact){
     self.contacts.push(contact)
   }
+  self.checkclick = function(){
+
+  }
+
 })
+.controller('AddContactController' , function ($scope,contactService){
+    $scope.save = function () {
+      var contact = { text: $scope.text}
+      contactService.add(contact)
+      resetform()
+      function resetform(){
+       $scope.text = ''
+      }
+     }
+    }
