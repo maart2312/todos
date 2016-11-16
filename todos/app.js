37c6edc3260008f3bc3ab6656ec9222b80d4ef93
Napat Wannachai <napatwannachai@hotmail.com>
2016-11-17 00:46:47 +0700

    Add controller Listtodocon

--- todos/app.js
+++ todos/app.js
@@ -1,26 +1,29 @@
 angular.module('ContactApp', [])
-.service('contactService', function (){
-  var self = this
-  self.contacts = [
-]
-  self.list = function (){
-    return self.contacts
-  }
-  self.add =  function(contact){
-    self.contacts.push(contact)
-  }
-  self.checkclick = function(){
+  .service('contactService', function (){
+    var self = this
+    self.contacts = [
+    ]
+    self.list = function (){
+      return self.contacts
+    }
+    self.add =  function(contact){
+      self.contacts.push(contact)
+    }
+    self.checkclick = function(){
 
-  }
+    }
 
-})
-.controller('AddContactController' , function ($scope,contactService){
+  })
+  .controller('AddContactController' , function ($scope,contactService){
     $scope.save = function () {
       var contact = { text: $scope.text}
       contactService.add(contact)
       resetform()
-      function resetform(){
-       $scope.text = ''
-      }
-     }
     }
+    function resetform(){
+     $scope.text = ''
+    }
+  })
+  .controller('ListTodoController', function ($scope,contactService){
+      $scope.contacts = contactService.list()
+  })
